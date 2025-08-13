8300af7c2af16ab781a7f57a72d15784c940a0fe
ahmoin <123082059+ahmoin@users.noreply.github.com>
2025-08-13 13:59:35 -0400

    fix: change with google oauth text dependent on auth flow

--- app/page.tsx
+++ app/page.tsx
@@ -651,7 +651,7 @@ export default function Home() {
 											fill="currentColor"
 										/>
 									</svg>
-									Login with Google
+									{authFlow === "signIn" ? "Login" : "Sign up"}. with Google
 								</Button>
 								<div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
 									<span className="relative z-10 bg-background px-2 text-muted-foreground">
